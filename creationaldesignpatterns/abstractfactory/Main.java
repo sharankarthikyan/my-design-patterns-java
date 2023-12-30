99eccfbe11846be7d4e0eff19e85c47fdb2d829f
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-12-30 22:48:36 +0530

    variable name change in main method of abstract factory

--- creationaldesignpatterns/abstractfactory/Main.java
+++ creationaldesignpatterns/abstractfactory/Main.java
@@ -2,16 +2,16 @@ package creationaldesignpatterns.abstractfactory;
 
 public class Main {
   public static void main(String[] args) {
-    AbstractFactory shapeFactory1 = FactoryProducer.getFactory(false);
-    Shape shape1 = shapeFactory1.getShape("RECTANGLE");
+    AbstractFactory shapeFactory = FactoryProducer.getFactory(false);
+    Shape shape1 = shapeFactory.getShape("RECTANGLE");
     shape1.draw();
-    Shape shape2 = shapeFactory1.getShape("SQUARE");
+    Shape shape2 = shapeFactory.getShape("SQUARE");
     shape2.draw();
 
-    AbstractFactory shapeFactory2 = FactoryProducer.getFactory(true);
-    Shape shape3 = shapeFactory2.getShape("RECTANGLE");
+    AbstractFactory roundedShapeFactory = FactoryProducer.getFactory(true);
+    Shape shape3 = roundedShapeFactory.getShape("RECTANGLE");
     shape3.draw();
-    Shape shape4 = shapeFactory2.getShape("SQUARE");
+    Shape shape4 = roundedShapeFactory.getShape("SQUARE");
     shape4.draw();
   }
 }
