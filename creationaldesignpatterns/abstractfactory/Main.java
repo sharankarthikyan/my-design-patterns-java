9bfab8c478dc056d04bd94cf4595c79096093438
sharankarthikeyan <karthikeyansharan00@gmail.com>
2024-01-16 16:43:08 +0530

    added comments for creational patterns

--- creationaldesignpatterns/abstractfactory/Main.java
+++ creationaldesignpatterns/abstractfactory/Main.java
@@ -1,5 +1,15 @@
 package creationaldesignpatterns.abstractfactory;
 
+/*
+ * Abstract Factory is a creational design pattern that lets you 
+ * produce families of related objects without specifying their 
+ * concrete classes.
+ * 
+ * Use the Abstract Factory when your code needs to work with various 
+ * families of related products, but you don’t want it to depend on 
+ * the concrete classes of those products—they might be unknown beforehand 
+ * or you simply want to allow for future extensibility.
+ */
 public class Main {
   public static void main(String[] args) {
     AbstractFactory shapeFactory = FactoryProducer.getFactory(false);
--- creationaldesignpatterns/builder/Main.java
+++ creationaldesignpatterns/builder/Main.java
@@ -1,5 +1,18 @@
 package creationaldesignpatterns.builder;
 
+/*
+ * Builder is a creational design pattern that lets you construct 
+ * complex objects step by step. The pattern allows you to produce 
+ * different types and representations of an object using the same 
+ * construction code.
+ * 
+ * Use the Builder pattern when you want your code to be able to 
+ * create different representations of some product (for example, 
+ * stone and wooden houses).
+ * 
+ * Use the Builder to construct Composite trees or other complex 
+ * objects.
+ */
 public class Main {
   public static void main(String[] args) {
     MealBuilder mealBuilder = new MealBuilder();
